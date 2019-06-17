072c98c7f7debb2a4943492811e5a8813545dc1f
github username <github email address>
2019-06-17 16:30:04 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # Add route from Readme
+  get '/birds' => 'birds#index'
+  #Any visitors to the '/birds' will get routed to the index action in the BirdsController. This action shows one thing - get all instances of the Bird model and store them in a variable @birds. 
 end
